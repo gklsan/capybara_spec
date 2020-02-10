88e63f14c6d1fffe24538fc049653893c0db2ecb
Gokul <pgokulmca@gmail.com>
2020-02-10 22:12:37 +0530

    form spec updated

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "Forms", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'can enter a name and receive a greeting' do
+    visit '/'
+
+    fill_in :name, with: "Capybara"
+    click_on 'submit'
+    expect(page).to have_content "Welcome, Capybara!"
+  end
 end
